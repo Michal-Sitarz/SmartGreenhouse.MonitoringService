0a97d63bb9aa30ac861e0a936e0248b0a229f4dd
Michal-Sitarz <me@michalsitarz.com>
2021-08-08 11:24:10 +0100

    Implement main algorithm. Add Http requests to Rest Web API. Add model for JSON serialization.

---
+++ SmartGreenhouse.MonitoringService/ConditionsReading.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartGreenhouse.MonitoringService
+{
+    public class ConditionsReading
+    {
+        public double AirTemperature { get; set; }
+        public double AirHumidity { get; set; }
+        public double SoilMoisture { get; set; }
+        public int LightIntensity { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string SensorNodeId { get; set; }
+    }
+}
--- SmartGreenhouse.MonitoringService/Program.cs
+++ SmartGreenhouse.MonitoringService/Program.cs
@@ -2,61 +2,133 @@
 using System.Collections.Generic;
 using System.Net.Mqtt;
 using System.Text;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Net.Http.Headers;
 
 namespace SmartGreenhouse.MonitoringService
 {
     class Program
     {
+        static List<ConditionsReading> averageConditions = new();
+        static HttpClient httpClient = new();
+
         static void Main(string[] args)
         {
             // initial setup: parameters
+            const string _webApi = "http://192.168.1.132:8888/";
             const string _mqttBroker = "192.168.1.98";
             const string _mqttClientId = "MonitoringService";
-            List<string> topics = new List<string> { "greenhouse/conditions", "anotherTopic" };
+            List<string> topics = new() { "greenhouse/conditions" };
 
             // initial setup: initialize
             while (true)
             {
                 Console.WriteLine("Starting application...");
 
+                // setup: http client for web api access
+                httpClient.BaseAddress = new Uri(_webApi);
+                httpClient.DefaultRequestHeaders.Accept.Clear();
+                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                // setup: mqtt broker to handle publish-subscribe messages
                 var mqtt = StartMqttClientAsync(_mqttBroker, _mqttClientId).Result;
 
                 if (mqtt.connected)
                 {
                     // subscribe to all topics
-                    var subscribed = SubscribeToTopics(topics, mqtt.client).Result;
+                    var subscribed = SubscribeToTopicsAsync(topics, mqtt.client).Result;
                     if (subscribed)
                     {
-                        Console.WriteLine("Succesfully subscribed to all topics.");
                         // recieve messages from topics
                         mqtt.client
                             .MessageStream
-                            .Subscribe(msg => MessagesMonitoring(msg.Topic, Encoding.UTF8.GetString(msg.Payload)));
-                        //   ^ observable collection, triggered whenever new message is recieved from any topic
+                            .Subscribe(msg => MessagesMonitoring(msg.Topic, Encoding.UTF8.GetString(msg.Payload), mqtt.client));
+                        // ^ observable collection, triggered whenever new message is recieved from any topic
                     }
 
                     while (true)
                     {
-                        // keep application alive = in a constant loop
+                        // constant loop to keep application alive
                     }
                 }
             }
         }
 
-        static void MessagesMonitoring(string topic, string message)
+        static void MessagesMonitoring(string topic, string message, IMqttClient mqttClient)
         {
-            // logic what to do with each type (topic) of a message
-            // i.e. if this topic, then this...
+            try
+            {
+                // deserialize recieved message into an object (model)
+                ConditionsReading cr = JsonSerializer.Deserialize<ConditionsReading>(message);
 
-            // for now, for all new messages -> display in console
-            Console.WriteLine();
-            Console.Write("Message payload: ");
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.Write(message);
-            Console.ResetColor();
-            Console.Write($" [@ Topic: {topic}]");
+                // add datetime stamp
+                cr.TimeStamp = DateTime.Now;
+
+                // display in a console
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write($"Temperature: {cr.AirTemperature} Humidity: {cr.AirHumidity}");
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.Write($" Sensor: {cr.SensorNodeId}");
+                Console.ResetColor();
+                Console.Write($" @ {cr.TimeStamp}");
+                Console.WriteLine();
+
+                // save conditions reading in DB
+                var response = PostConditionsToWebApiAsync(cr, message);
+                if (response.Result.IsSuccessStatusCode)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine("Http Request with data sent succesfully to the Web API.");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("Http Request not sent!!!");
+                    Console.ResetColor();
+                    Console.WriteLine(response);
+                }
+
+                // calculate average conditions
+                var avgConditions = CalculateAverageConditions(cr);
+                avgConditions.TimeStamp = DateTime.Now;
+                avgConditions.SensorNodeId = "averaged";
+
+                // publish average conditions
+                var msgAvgConditions = JsonSerializer.Serialize(avgConditions);
+                var tryPublish = PublishMessageAsync(msgAvgConditions, "greenhouse/averageconditions", mqttClient);
+                if (tryPublish.Result)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkBlue;
+                    Console.WriteLine("Average conditions published to the MQTT broker successfully.");
+                    Console.ResetColor();
+                }
+
+            }
+            #region Catch and Report Exceptions
+            catch (Exception ex)
+            {
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Failed to process the message:");
+                Console.ResetColor();
+                Console.WriteLine(message);
+
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Error:");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex);
+                Console.ResetColor();
+                Console.WriteLine();
+                return;
+            }
+            #endregion
         }
 
         static void Publish(IMqttClient mqttClient)
@@ -115,7 +187,7 @@ namespace SmartGreenhouse.MonitoringService
             }
         }
 
-        static async Task<bool> SubscribeToTopics(List<string> topics, IMqttClient client)
+        static async Task<bool> SubscribeToTopicsAsync(List<string> topics, IMqttClient client)
         {
             try
             {
@@ -124,12 +196,71 @@ namespace SmartGreenhouse.MonitoringService
                     await client.SubscribeAsync(t, MqttQualityOfService.AtLeastOnce);
                 }
 
+                Console.WriteLine("Succesfully subscribed to all topics.");
                 return true;
             }
             catch (Exception)
             {
+                Console.WriteLine("Subscribing to topics failed...");
                 return false;
             }
+        }
+
+        static async Task<HttpResponseMessage> PostConditionsToWebApiAsync(ConditionsReading cr, string msg)
+        {
+            //var jsonCr = JsonSerializer.Serialize(cr);
+            //var myJson = msg;
+            var response = await httpClient.PostAsJsonAsync("api/conditionsreadings", cr);
+            //var response = await httpClient.PostAsync("api/conditionsreadings", new StringContent(myJson, Encoding.UTF8, "application/json"));
+
+            return response;
+        }
+
+        static ConditionsReading CalculateAverageConditions(ConditionsReading cr)
+        {
+            if (averageConditions.Where(x => x.SensorNodeId == cr.SensorNodeId).Any())
+            {
+                averageConditions.RemoveAll(x => x.SensorNodeId == cr.SensorNodeId);
+            }
+            averageConditions.Add(cr);
+
+            var avgCR = new ConditionsReading();
+
+            int airTemperatureCounter = 0;
+            int airHumidityCounter = 0;
+            int soilMoistureCounter = 0;
+            int lightIntensityCounter = 0;
+
+            foreach (var reading in averageConditions)
+            {
+                if (reading.AirTemperature != 0)
+                {
+                    airTemperatureCounter++;
+                    avgCR.AirTemperature += reading.AirTemperature;
+                }
+                if (reading.AirHumidity != 0)
+                {
+                    airHumidityCounter++;
+                    avgCR.AirHumidity += reading.AirHumidity;
+                }
+                if (reading.SoilMoisture != 0)
+                {
+                    soilMoistureCounter++;
+                    avgCR.SoilMoisture += reading.SoilMoisture;
+                }
+                if (reading.LightIntensity != 0)
+                {
+                    lightIntensityCounter++;
+                    avgCR.LightIntensity += reading.LightIntensity;
+                }
+            }
+
+            if (avgCR.AirTemperature != 0) avgCR.AirTemperature = Math.Round(avgCR.AirTemperature / airTemperatureCounter, 2);
+            if (avgCR.AirHumidity != 0) avgCR.AirHumidity = Math.Round(avgCR.AirHumidity / airHumidityCounter, 2);
+            if (avgCR.SoilMoisture != 0) avgCR.SoilMoisture = Math.Round(avgCR.SoilMoisture / soilMoistureCounter, 2);
+            if (avgCR.LightIntensity != 0) avgCR.LightIntensity = avgCR.LightIntensity / lightIntensityCounter;
+
+            return avgCR;
         }
 
     }
